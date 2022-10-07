68fee76c4b26c0f4d6beb94d59cdbac98da84a33
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2022-10-06 19:40:38 -0700

    review

---
+++ class-10/review/review-notes.md
@@ -0,0 +1,33 @@
+# Review
+
+- We MUST be in Remo during final
+- DO NOT LEAVE until you meet with me
+- PASSING = 16 or more points
+- OPEN BOOK FINAL EXAM
+  - You can use EVERY resource available except other humans
+
+## Functions
+
+## Loops
+
+## Tools To Use During Final
+
+- Review Notes
+- [Repl review](https://replit.com/@KassieBradshaw/102n65-Review)
+- [codepen review](https://codepen.io/kassiebradshaw/pen/eYrLKJp)
+
+- [Use Repl](https://www.replit.com)
+- [Use codepen.io](https://codepen.io/your-work)
+
+## CSS Selectors
+
+- Select by element: `element-name{}`
+- Select by class: `.class-value{}`
+- Select by id: `#id-name{}`
+- Universal selector: `*{}`
+
+## Comparison Operators
+
+- `>` & `>=` there is a difference
+
+## Quiz 7 Q5
---
+++ class-10/review/review.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// ----------------- LOOPS -----------------------------------------------
+
+// FOR loop <-- when we know ahead of time how many iterations we need
+    // For every item in a shopping cart
+    // For every book on a bookshelf
+
+// STRUCTURE of a For loop
+// for (initial value; condition to evaluate [must be true]; increment){
+//    code to execute goes here;
+// }
+
+// ------------------------------------------------------------------------------
+
+// WHILE loop <-- when we don't know how many iterations we will need
+    // Getting the user to give us the correct input
+    // Passwords, guessing a correct answer, etc
+
+// STRUCTURE of a While loop
+
+// while (this condition evaluates to true){
+//    execute this code  
+// }
+
+// ---------------------- FUNCTIONS   ---------------------------------------------------
+
+// STRUCTURE OF A FUNCTION
+
+// DECLARING/DEFINING your function (creating it):
+
+      // function functionName(parameters go here){
+      //    code to execute here;
+      // }
+
+// INVOKING/CALLING function (using it);
+      // functionName(arguments go here);
+
+// Function expression (dangerous):
+
+// let myFunction = function(){
+//   code to execute here;
+// }
+
